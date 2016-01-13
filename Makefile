954df520e4c1b02c02ff75fe429d38b5d9a6fc8e
katzchang <katzchang@gmail.com>
2016-01-13 17:45:20 +0900

    hoge-

--- Makefile
+++ Makefile
@@ -3,9 +3,16 @@
 run: main.go
 	go run $<
 
-curl:
+curl: index foo bar
+
+index:
 	curl -i "localhost:8081"
+
+foo:
 	curl -i "localhost:8081/foo"
 	curl -i "localhost:8081/foo?a=hoge"
 
-
+bar:
+	curl -i "localhost:8081/bar?a=%7B%22n%22%3A%22Alice%22%2C%22b%22%3A%22hige-%22%7D"
+	curl -i "localhost:8081/bar?a="
+	curl -i "localhost:8081/bar"
--- main.go
+++ main.go
@@ -3,34 +3,50 @@ package main
 import (
 	"fmt"
 //	"html"
+	"net/url"
 	"net/http"
 	"encoding/json"
 )
 
 func main() {
-	http.HandleFunc("/foo", handler)
+	http.HandleFunc("/foo", foo)
+	http.HandleFunc("/bar", bar)
 	http.ListenAndServe(":8081", nil)
 }
 
 type Message struct {
-	Name string `json:"name"`
-	BodyHoge string `json:"body_hoge"`
-	Time int64 `json:"time"`
+	Name string `json:"n"`
+	Body string `json:"b"`
 }
 
 func hoge(s string) string {
-	m := Message{"Alice", s, 1294706395881547000}
+	m := Message{"Alice", s}
 	b, _ := json.Marshal(m)
 	return string(b)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func foo(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Query().Get("a") == "hoge" {
-		w.Header().Set("Content-Type","application/json; charset=utf-8")
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintln(w, string(hoge("error!!!!")), )
 	} else {
 		//	fmt.Fprintf(w, "Hello, %q\n", html.EscapeString(r.URL.Path))
-		fmt.Fprintln(w, hoge("hoge-"))
+		fmt.Fprintln(w, url.QueryEscape(hoge("hoge-")))
 	}
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	a := query.Get("a")
+	var m Message
+	if err := json.Unmarshal([]byte(a), &m); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, string(hoge("error!!!!")), )
+	} else {
+		b, _ := json.Marshal(m)
+		fmt.Fprintln(w, string(b))
+	}
+
 }
